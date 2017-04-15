8725c52e0ac5f01ba9ba71cb2ce1c25861cd2f44
MichaelTonyFerrari <ferraro2ndbase@gmail.com>
2017-04-15 18:51:47 -0400

    updated file

--- envronments/Strings.py
+++ envronments/Strings.py
@@ -1,4 +1,14 @@
 types_of_people = 10
-x = f"There are {types_of_people} types of people"
+x = "There are", {types_of_people}, "types of people"
 
 print(x)
+
+y = "sup you bros"
+
+print(y)
+
+joke = "True or false, is this joke funny? {}"
+halarious = False; 
+
+
+print(joke.format(halarious))
