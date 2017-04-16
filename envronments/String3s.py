81f67c7021772587820fd0547adda1eb53babc2f
restores <ferraro2ndbase@gmail.com>
2017-04-16 09:35:42 -0400

    Adding new line and triple quote printing

---
+++ envronments/String3s.py
@@ -0,0 +1,11 @@
+days = "Mon Tues Wed Thur Fri Sat Sun"
+
+months = "Jan\nFeb\nMarch\nApril\nMay\nJune\nJuly\nAug\nSept\nOct\nNov\nDec"
+
+print("Here are the days", days)
+print("Here are the months", months)
+
+print (""" What is happening here?
+i cant just print as much as i want?
+thats pretty sick bro
+""")
