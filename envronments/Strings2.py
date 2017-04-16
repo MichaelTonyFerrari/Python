7dbdd3364682a821ec31b73a22ffc15909b7f454
MichaelTonyFerrari <ferraro2ndbase@gmail.com>
2017-04-15 22:19:58 -0400

    finished

---
+++ envronments/Strings2.py
@@ -0,0 +1,18 @@
+types_of_people = 10
+
+x = f'There are {types_of_people} types of people'
+
+binary = 'Binary'
+do_not = "Don't"
+
+y = f"Those who know {binary} and those who {do_not}"
+
+hilarious = False 
+joke_eval = 'Isnt that joke so funny?!{}'
+
+print(joke_eval.format(hilarious))
+
+left = 'This is the left side of the string and ...'
+right = 'this is the right side of string'
+
+print(left+right)
