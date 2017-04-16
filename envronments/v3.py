09d4260f55b3690daa1f5edf9b00ff3972f182e2
MichaelTonyFerrari <ferraro2ndbase@gmail.com>
2017-04-15 22:10:31 -0400

    updated

--- envronments/v3.py
+++ envronments/v3.py
@@ -7,3 +7,9 @@ my_hair = 'Brown'
 my_teeth = 'White'
 
 print(f'Lets talk about {my_name}')
+print(f'Hes {my_weight} pounds')
+print(f'Hes {my_height} inches tall')
+
+total = my_age + my_height + my_weight
+print(f'If i add {my_age} to {my_height} to {my_weight} i get total of {total}')
+print(f'If I add my age, height and weight i will recieve a total')
