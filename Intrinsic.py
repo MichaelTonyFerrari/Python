8959fd6c4afdf63358513754aff4ccc44fe8255d
restores <ferraro2ndbase@gmail.com>
2017-08-14 10:39:50 -0400

    arange function numpy and size returns

---
+++ Intrinsic.py
@@ -0,0 +1,80 @@
+
+# coding: utf-8
+
+# <h2>Intrinsic NumPy array creation using NumPy's methods</h2>
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[2]:
+
+
+# Create array of intergers 
+np.arange(5)
+
+
+# In[3]:
+
+
+np.arange(5, 20)
+
+
+# In[4]:
+
+
+np.arange(5, 20)-10
+
+
+# In[5]:
+
+
+np.arange(5, 20)-10+2
+
+
+# In[6]:
+
+
+len(np.arange(5, 20))
+
+
+# In[7]:
+
+
+range_arr = np.arange(5, 20)
+range_arr.size
+
+
+# In[8]:
+
+
+np.arange(5, 20).size
+
+
+# In[9]:
+
+
+# start, stop, step
+np.arange(4, 20, 2)
+
+
+# In[10]:
+
+
+np.arange(5, 20, 5)
+
+
+# In[11]:
+
+
+# stop, set step 
+np.arange(20, step =2)
+
+
+# In[ ]:
+
+
+
+
